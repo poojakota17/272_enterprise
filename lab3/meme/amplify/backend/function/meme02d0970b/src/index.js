349e23746b3c8763f40f9b5802b3c45070d6308f
anastaszi <anastasba@gmail.com>
2020-11-08 16:23:43 -0800

    updating lambda with cors

--- lab3/meme/amplify/backend/function/meme02d0970b/src/index.js
+++ lab3/meme/amplify/backend/function/meme02d0970b/src/index.js
@@ -9,9 +9,9 @@ exports.handler = async (event) => {
     const response = {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*"
-    //  }, 
+      headers: {
+          "Access-Control-Allow-Origin": "*"
+      }, 
         body: JSON.stringify('Hello from Lambda!'),
     };
     return response;
--- lab3/meme/src/components/testLambda.js
+++ lab3/meme/src/components/testLambda.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import Amplify, { API } from 'aws-amplify';
+import awsconfig from '../aws-exports';
+
+Amplify.configure(awsconfig);
 
 
 export default function TestLambda(props) {
   console.log(props)
   function handleSubmit() {
+    const apiName = 'updatememe';
+  const path = '/newmeme';
+  const myInit = { // OPTIONAL
+    headers: {}, // OPTIONAL
+  };
     console.log("you clicked me");
+
+API
+  .get(apiName, path, myInit)
+  .then(response => {
+    console.log(response)
+  })
+  .catch(error => {
+    console.log(error.response);
+ });
   }
 
   return (

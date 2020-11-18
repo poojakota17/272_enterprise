43f30ddf441804ab7223b414eba0d4cabe21ffd2
Manjiri 1101 <Manjiri1101@users.noreply.github.com>
2020-11-18 14:04:27 -0800

    node-modules is not updating

--- feedback/src/App.js
+++ feedback/src/App.js
@@ -4,8 +4,14 @@ import React, { useState, useEffect } from "react";
 import { Guest } from './views/Guest';
 import { UserPage } from './views/UserPage';
 
+ import { API } from 'aws-amplify';
+import { listFeedbacks } from './graphql/queries';
+import { createFeedback as createFeedbackMutation, deleteFeedback as deleteFeedbackMutation } from './graphql/mutations';
 function App() {
   const [user, setUser] = useState(null);
+  const [feedback, setFeedback] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
 
   useEffect(() => {
     (async function() {
@@ -17,10 +23,35 @@ function App() {
       catch {
         setUser(null);
       }
-    })();
+
+    }
+    
+    )();
+    fetchFeedbacks();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return user ? (<UserPage />) : (< Guest />);
+  async function fetchFeedbacks() {
+    const apiData = await API.graphql({ query: listFeedbacks });
+    setFeedback(apiData.data.listFeedbacks.items);
+  }
+  async function createFeedback() {
+    if (!formData.recipent || !formData.feedback) return;
+    await API.graphql({ query: createFeedbackMutation, variables: { input: formData } });
+    setFeedback([ ...feedback, formData ]);
+    setFormData(initialFormState);
+  }
+       
+
+  return (
+    <div>Hello User
+      <input
+        onChange={e => setFormData({ ...formData, 'feedback': e.target.value})}
+        placeholder="Please write the feedback"
+        value={formData.feedback}
+      />
+      <button onClick={createFeedback}>Submit</button>
+    </div>
+  );
 }
 
 export default App;
